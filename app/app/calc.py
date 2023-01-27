e83340810221e92a692afcd52ab183e71fc8a646
pawan yajaman <yajama77@students.rowan.edu>
2023-01-26 22:28:39 -0500

    database config half way

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator Functions
+"""
+
+
+def add(x,y):
+    """Add x and y and return result"""
+    return x + y
+
+def sub(x,y):
+    """Subtract x from y and return result"""
+    return y - x
