171d0a8162253b9d113509d576729f46771c34b6
pawan yajaman <yajama77@students.rowan.edu>
2023-01-26 22:32:54 -0500

    fix lint errors

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,19 +5,20 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
 
     def test_add_numbers(self):
         """Test adding numbers together"""
 
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
     def test_sub_numbers(self):
         """Test subract numbers"""
 
-        res = calc.sub(10,15)
+        res = calc.sub(10, 15)
 
-        self.assertEqual(res,5)
+        self.assertEqual(res, 5)
